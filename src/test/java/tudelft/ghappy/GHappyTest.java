c1841a37575ae35100a4ce11b20997ae92583413
Steve Losk <Stevedlosk@gmail.com>
2018-12-23 22:25:26 -0800

    Built tests for GHappy.  Fixed the bug in beginning 'g' test logic.

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,76 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    GHappy gHappy;
+
+
+    @Test
+    public void emptyString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void singleGString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("g");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void doubleGString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("gg");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void doubleGinMiddleString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("asklggsakll");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void doubleGEndingString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("asklggsakllgg");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void doubleGBeginningString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("ggasklggsakll");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void noGString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("adkjkdk");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void singleGonBeginningString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("gslkslkgglkas");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void singleGonEndingString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("slkslkgglkasg");
+        Assertions.assertFalse(result);
+    }
+
+    
 }
