0a61103f84fb23ee6c7d638f34ae991cd91938c1
Steve Losk <Stevedlosk@gmail.com>
2018-12-23 22:33:34 -0800

    Built tests for CaesarShift.  Fixed bug that appended old value in addition to new value.

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,43 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
 
+
+    @Test
+    public void singleCharShift2() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        String result = csc.CaesarShiftCipher("a", 2);
+        Assertions.assertEquals("c", result);
+    }
+
+    @Test
+    public void wrapAroundSingleCharShift2() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        String result = csc.CaesarShiftCipher("z", 2);
+        Assertions.assertEquals("b", result);
+    }
+
+    @Test
+    public void multipleCharShift4() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        String result = csc.CaesarShiftCipher("cdef", 4);
+        Assertions.assertEquals("ghij", result);
+    }
+
+    @Test
+    public void multipleCharShift4SomeWrapAround() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        String result = csc.CaesarShiftCipher("wxyz", 3);
+        Assertions.assertEquals("zabc", result);
+    }
+
+    @Test
+    public void singleCharShift29() {
+        CaesarShiftCipher csc = new CaesarShiftCipher();
+        String result = csc.CaesarShiftCipher("a", 29);
+        Assertions.assertEquals("d", result);
+    }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -72,5 +72,5 @@ public class GHappyTest {
         Assertions.assertFalse(result);
     }
 
-    
+
 }
