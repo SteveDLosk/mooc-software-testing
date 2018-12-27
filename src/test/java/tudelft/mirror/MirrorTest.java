1d1dcea9b16f87d1ac163c2f127a248af501679a
Steve Losk <Stevedlosk@gmail.com>
2018-12-26 19:59:34 -0800

    Built test cases for MirrorTest.

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,99 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    Mirror mirror;
+
+    @BeforeEach
+    public void initialize() {
+        mirror = new Mirror();
+    }
+
+    @Test
+    public void oddPalindrome() {
+        String string = "abcba";
+
+        String mirrorEnd = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(mirrorEnd, "abcba");
+    }
+
+    @Test
+    public void evenPalindrome() {
+        String string = "abba";
+
+        String mirrorEnd = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(mirrorEnd, "abba");
+    }
+
+
+
+    @Test
+    public void singleCharMirror() {
+        String string = "absebda";
+
+        String mirrorEnd = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(mirrorEnd, "a");
+    }
+
+    @Test
+    public void singleCharMirrorWithMirrorInMiddleAlso() {
+        String string = "abseeebda";
+
+        String mirrorEnd = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(mirrorEnd, "a");
+    }
+    @Test
+    public void twoCharMirror() {
+        String string = "abdfcba";
+
+        String mirrorEnd = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(mirrorEnd, "ab");
+    }
+
+    @Test
+    public void noMirror() {
+        String string = "abcde";
+
+        String mirrorEnd = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(mirrorEnd, "");
+    }
+
+    @Test
+    public void emptyString() {
+        String string = "";
+
+        String mirrorEnd = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(mirrorEnd, "");
+    }
+
+    @Test
+    public void mirroredButNotAtEnd() {
+        String string = "abcbazz";
+
+        String mirrorEnd = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(mirrorEnd, "");
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
